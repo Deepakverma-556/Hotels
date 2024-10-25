21fdf683753018a663592bdf6d62fba5d3c2534b
unknown <deepakradialcode@gmail.com>
2024-10-25 11:24:02 +0530

    add css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xsm:'12px',
+        xxl:'39px',
+        xxxl:'62px',
+      },
+      colors: {
+        'dark-blue':'#11142d',
+        'mustard':'#c7923e',
+        'gray':'#515151',
+        'light-gray':'#9a9ab0',
+      },
+      lineHeight: {
+        xl: '46px',
+        xxl:'74px',
+      },
+      fontFamily: {
+        lora:['Lora', 'sans-serif'],
+        mulish:['Mulish', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
